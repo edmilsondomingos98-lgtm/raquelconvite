ccb71372b787c1de8418da86f36119cac43f61e5
edmilsondomingos98-lgtm <edmilsondomingos98@gmail.com>
2026-07-15 01:24:57 +0100

    Add files via upload

---
+++ party.ts
@@ -0,0 +1,19 @@
+export const PARTY = {
+  nome: "Raquel da Silva",
+  primeiroNome: "Raquel",
+  idade: 16,
+  diaSemana: "Domingo",
+  data: "09 de Agosto",
+  horario: "12:00",
+  local: "Jardim dos Eventos",
+  localDetalhe:
+    "Depois da cidade da China, atrás da bomba de combustível da Sonangol, em Viana.",
+  dressCode: "É obrigatório o uso de roupa branca ou creme",
+  mapsLink: "https://maps.app.goo.gl/KxtVo8anF85qVPXS6?g_st=aw",
+  mapsEmbedQuery:
+    "Sal%C3%A3o%20de%20Festas%20-%20Jardim%20dos%20Eventos%2C%20Viana%2C%20Angola",
+  contactos: [
+    { nome: "Osvaldo da Silva", papel: "Pai", telefone: "946 441 937" },
+    { nome: "Glória da Silva", papel: "Mãe", telefone: "923 936 907" },
+  ],
+};
